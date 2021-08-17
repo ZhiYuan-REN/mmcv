c912fdf0bf963a0893655cd7b4c10ae9d807e118
ZhiYuan-REN <3295715438@qq.com>
2021-08-17 16:15:48 +0800

    fix one redundant code

--- mmcv/utils/registry.py
+++ mmcv/utils/registry.py
@@ -20,7 +20,7 @@ def build_from_cfg(cfg, registry, default_args=None):
     if not isinstance(cfg, dict):
         raise TypeError(f'cfg must be a dict, but got {type(cfg)}')
     if 'type' not in cfg:
-        if default_args is None or 'type' not in default_args:
+        if 'type' not in default_args:
             raise KeyError(
                 '`cfg` or `default_args` must contain the key "type", '
                 f'but got {cfg}\n{default_args}')
